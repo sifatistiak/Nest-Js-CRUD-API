46f93c3cb9f079c1a035742c0fd5e8e4cbbbffb9
Istiak Ahamed <sifatistiak@gmail.com>
2022-07-31 03:43:02 +0600

    eslint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   signup() {
-    return { msg: "This Is Sign Up" };
+    return { msg: 'This Is Sign Up' };
   }
   signin() {
-    return { msg: "This Is Sign In" };
+    return { msg: 'This Is Sign In' };
   }
 }

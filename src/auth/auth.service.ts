57ab73f607b70cb3d2ad65f1a00448692606670a
Istiak Ahamed <sifatistiak@gmail.com>
2022-08-01 01:01:18 +0600

    Sign Up Complete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'This Is Sign Up' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Duplicate Email');
+        }
+      }
+      throw error;
+    }
   }
   signin() {
     return { msg: 'This Is Sign In' };

06b50693d3b399d87a2a83c1ca0b3d301f53e866
Istiak Ahamed <sifatistiak@gmail.com>
2022-08-21 23:40:09 +0600

    Return User Info by Prisma

--- src/auth/stratagy/jwt.strategy.ts
+++ src/auth/stratagy/jwt.strategy.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +14,16 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+  async validate(payload: {
+    sub: number,
+    email: string,
+  }) {
+    const user = await this.prisma.user.findUnique({
+        where: {
+            id: payload.sub,
+        },
+    });
+    delete user.hash;
+    return user;
   }
 }
